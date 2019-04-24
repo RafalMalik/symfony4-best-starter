58ce12334f0e87412a0e2a713b7b5733815ee34b
RafalMalik <rafalmalik.info@gmail.com>
2019-04-24 23:26:08 +0200

    Create login form.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -19,23 +21,15 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/login", name="app_login")
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
-    }
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout()
-    {
-        // controller can be blank: it will never be executed!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
 
