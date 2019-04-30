caa5445cbb5d5c2169b3033d78755b8d75a991c7
RafalMalik <rafalmalik.info@gmail.com>
2019-04-30 21:15:06 +0200

    Create UserController and action boilerplate.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class UserController extends AbstractController
 {
     /**
@@ -16,4 +17,37 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+
+    /**
+     * @todo Make action when admin can generate user,
+     */
+    public function new() {
+
+    }
+
+    /**
+     * @todo Make show action, some like profile when shown all users data and link to edit profile.
+     */
+    public function show() {
+
+    }
+
+    /**
+     * @todo Make edit action, when we have form with user data and can edit it.
+     */
+    public function edit() {
+
+    }
+
+    /**
+     * @todo Make delete user action -> delete user with all related data.
+     */
+    public function delete() {
+
+    }
+
+
+
+
 }
