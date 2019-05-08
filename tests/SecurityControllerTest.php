e4bdb46139916fc1db59b52f08840d6870cbf6e8
RafalMalik <rafalmalik.info@gmail.com>
2019-05-08 21:05:38 +0200

    Write security tests.

--- tests/SecurityControllerTest.php
+++ tests/SecurityControllerTest.php
@@ -7,7 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class SecurityControllerTest extends WebTestCase
 {
 
-    public function testSuccessLogin()
+    /**
+     * Check that user successfully login with valid credentials.
+     *
+     * @dataProvider successLoginProvider
+     * @param $credentials
+     */
+    public function testSuccessLogin($credentials)
     {
         $client = static::createClient(array(), array(
             'HTTP_HOST'       => 'localhost:1182',
@@ -18,20 +24,32 @@ class SecurityControllerTest extends WebTestCase
         $this->assertSame(200, $client->getResponse()->getStatusCode());
 
         $client->submitForm('Sign in', [
-            'email' => 'jano',
-            'password' => 'my safe password',
+            'email' => $credentials['email'],
+            'password' => $credentials['password'],
         ]);
 
         $crawler = $client->followRedirect();
 
         $this->assertGreaterThan(
             0,
-            $crawler->filter('html:contains("Email could not be found.")')->count()
+            $crawler->filter('html:contains("Hello AppController")')->count()
         );
     }
 
 
-    public function testFailedLogin()
+    public function successLoginProvider()
+    {
+        yield [['email' => 'huj@o2.pl', 'password' => '123456']];
+        yield [['email' => 'dyzio@o2.pl', 'password' => '123456']];
+    }
+
+
+    /**
+     * Check that user cannot login with invalid credentials.
+     *
+     * @dataProvider failedLoginProvider
+     */
+    public function testFailedLogin($credentials)
     {
         $client = static::createClient(array(), array(
             'HTTP_HOST'       => 'localhost:1182',
@@ -42,18 +60,27 @@ class SecurityControllerTest extends WebTestCase
         $this->assertSame(200, $client->getResponse()->getStatusCode());
 
         $client->submitForm('Sign in', [
-            'email' => 'jano',
-            'password' => 'my safe password',
+            'email' => $credentials['email'],
+            'password' => $credentials['password'],
         ]);
 
         $crawler = $client->followRedirect();
 
         $this->assertGreaterThan(
             0,
-            $crawler->filter('html:contains("Email could not be found.")')->count()
+            $crawler->filter('html:contains("Email could not be found.")')->count() +
+            $crawler->filter('html:contains("Invalid credentials.")')->count()
         );
+    }
 
-
+    public function failedLoginProvider()
+    {
+        yield [['email' => 'huj1@o2.pl', 'password' => '123456']];
+        yield [['email' => 'huj2@o2.pl', 'password' => '123456']];
+        yield [['email' => 'huj3@o2.pl', 'password' => '123456']];
+        yield [['email' => 'huj4@o2.pl', 'password' => '123456']];
+        yield [['email' => 'huj5@o2.pl', 'password' => '123456']];
+        yield [['email' => 'huj@o2.pl', 'password' => '12345']];
     }
 
 }
