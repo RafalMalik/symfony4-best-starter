ac8f1a2dde87b3059b058bd8a5a6fca3c2178e9a
RafalMalik <rafalmalik.info@gmail.com>
2019-05-26 16:42:22 +0200

    Create tests to ProfileController.

--- tests/ProfileControllerTest.php
+++ tests/ProfileControllerTest.php
@@ -15,6 +15,14 @@ class ProfileControllerTest extends WebTestCase
     /** @var Client */
     private $client = null;
 
+    /** @var User $user  */
+    private $user = null;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
     /**
      * Initialize tests client with specific docker port
      */
@@ -23,46 +31,40 @@ class ProfileControllerTest extends WebTestCase
         $this->client = static::createClient(array(), array(
             'HTTP_HOST'       => 'localhost:1182',
         ));
+
+        $this->em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
     }
 
     public function logIn() {
         $session = $this->client->getContainer()->get('session');
 
-        /**
-         * @var EntityManagerInterface
-         */
-        $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
-
-
-        $user = $em->getRepository(User::class)->find(3);
-
-
-
-
+        $this->user = $this->em->getRepository(User::class)->find(3);
 
         $firewallName = 'main';
         // if you don't define multiple connected firewalls, the context defaults to the firewall name
         // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
 
-
         // you may need to use a different token class depending on your application.
         // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
-        $token = new UsernamePasswordToken($user, null, $firewallName, $user->getRoles());
+        $token = new UsernamePasswordToken($this->user, null, $firewallName, $this->user->getRoles());
         $this->client->getContainer()->get('security.token_storage')->setToken($token);
         $this->client->getContainer()->get('session')->set('_security_main', serialize($token));
 
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
+
+        /* Go to profile page and check status = 200 */
+        $this->client->request('GET', '/');
+
+        /* Handle redirect after success login to app/index page */
+        $crawler = $this->client->followRedirect();
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("app/index")')->count()
+        );
     }
-
-
-    /**
-     * @todo All small init tests extract to method and execute after logIn.
-     */
-    public function basicTests() {
-
-    }
-
 
 
     /**
@@ -71,25 +73,50 @@ class ProfileControllerTest extends WebTestCase
     public function testProfile()
     {
         /* LogIn to application */
+        $this->logIn();
 
         /* Go to profile page and check status = 200 */
-
-        /* Check that h1 contains "profile/index" */
-
-        /* Search user email in h3 tag */
-
-        /* Go to profile with parameter = 4 */
-
-        /* Check status = 200 */
-
-        /* Check h3 tag contains different email address */
-
-
-        $this->logIn();
-        /* Go to login page and check that status = 200 */
         $this->client->request('GET', '/profile');
+
+        /* Handle redirect after success login to app/index page */
+        $crawler = $this->client->followRedirect();
+
+        /* Check that response status is HTTP::OK */
         $this->assertSame(200, $this->client->getResponse()->getStatusCode());
 
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("profile/index")')->count()
+        );
+
+        /* Search user email in h3 tag */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("' . $this->user->getEmail() . '")')->count()
+        );
+
+        /* Go to profile with parameter = 4 */
+        /* Go to profile page and check status = 200 */
+        $this->client->request('GET', '/profile/4');
+        $crawler = $this->client->followRedirect();
+
+        /* Check status = 200 */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check h3 tag contains different email address */
+        $this->assertEquals(
+            0,
+            $crawler->filter('html:contains("' . $this->user->getEmail() . '")')->count()
+        );
+
+        /* Check h3 tag contains different email address */
+        $user = $this->em->getRepository(User::class)->find(3);
+
+        $this->assertEquals(
+            0,
+            $crawler->filter('html:contains("' . $user->getEmail() . '")')->count()
+        );
     }
 
     /**
@@ -99,12 +126,31 @@ class ProfileControllerTest extends WebTestCase
     public function testProfileFailedEdit()
     {
         /* LogIn to application */
+        $this->logIn();
 
         /* Go to profile page and check status = 200 */
+        $this->client->request('GET', '/profile/edit');
 
-        /* Check that h1 contains "profile/index" */
+        /* Handle redirect after success login to app/index page */
+        $crawler = $this->client->followRedirect();
+
+        /* Check that response status is HTTP::OK */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("profile/edit")')->count()
+        );
+
+        /* Search user email in h3 tag */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('#profile_email:contains("' . $this->user->getEmail() . '")')->count()
+        );
 
         /* Check that data in form are real and equals the log in user data */
+
 
         /* Change email value to invalid - phrase without @ (at) symbol */
 
