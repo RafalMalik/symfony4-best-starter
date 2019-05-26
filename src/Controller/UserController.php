d66d81199ce07f183a6f17a529fe22165b15f61b
RafalMalik <rafalmalik.info@gmail.com>
2019-05-26 17:29:06 +0200

    First UserController TDD iteration.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,52 +2,74 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+/**
+ * @Route("/user")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="app_user_index")
+     * @param UserRepository $userRepository
+     * @return Response
      */
-    public function index()
+    public function index(UserRepository $userRepository)
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            $users = $userRepository->findAll()
         ]);
     }
 
 
     /**
+     * @Route("/new", name="app_user_new")
+     *
      * @todo Make action when admin can generate user,
      */
     public function new() {
-
+        return $this->render('user/new.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
     }
 
     /**
+     * @Route("/show", name="app_user_show")
+     *
      * @todo Make show action, some like profile when shown all users data and link to edit profile.
      */
     public function show() {
+        return $this->render('user/show.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
 
     }
 
     /**
+     * @Route("/edit", name="app_user_edit")
+     *
      * @todo Make edit action, when we have form with user data and can edit it.
      */
     public function edit() {
 
+        return $this->render('user/edit.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
     }
 
     /**
+     * @Route("/delete", name="app_user_delete")
+     *
      * @todo Make delete user action -> delete user with all related data.
      */
     public function delete() {
-
+        return $this->render('user/delete.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
     }
-
-
-
 
 }
--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -2,9 +2,170 @@
 
 namespace App\Tests;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class UserControllerTest extends WebTestCase
 {
+
+    /** @var Client */
+    private $client = null;
+
+    /** @var User $user */
+    private $user = null;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * Initialize tests client with specific docker port
+     */
+    public function setUp()
+    {
+        $this->client = static::createClient(array(), array(
+            'HTTP_HOST' => 'localhost:1182',
+        ));
+
+        $this->em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+    }
+
+    public function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $this->user = $this->em->getRepository(User::class)->find(3);
+
+        $firewallName = 'main';
+
+        $token = new UsernamePasswordToken($this->user, null, $firewallName, $this->user->getRoles());
+        $this->client->getContainer()->get('security.token_storage')->setToken($token);
+        $this->client->getContainer()->get('session')->set('_security_main', serialize($token));
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+
+        /* Go to profile page and check status = 200 */
+        $crawler = $this->client->request('GET', '/');
+
+        /* Handle redirect after success login to app/index page */
+        //$crawler = $this->client->followRedirect();
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("app/index")')->count()
+        );
+    }
+
+
+    /**
+     * Test to user/index action
+     */
+    public function testIndex()
+    {
+        /* LogIn to application */
+        $this->logIn();
+
+        /* Go to profile page and check status = 200 */
+        $crawler = $this->client->request('GET', '/user');
+
+        /* Check that response status is HTTP::OK */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("user/index")')->count()
+        );
+    }
+
+
+    public function testNew()
+    {
+        /* LogIn to application */
+        $this->logIn();
+
+        /* Go to profile page and check status = 200 */
+        $crawler = $this->client->request('GET', '/user/new');
+
+        /* Check that response status is HTTP::OK */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("user/new")')->count()
+        );
+    }
+
+    /**
+     * Test to user/show action
+     */
+    public function testShow()
+    {
+        /* LogIn to application */
+        $this->logIn();
+
+        /* Go to profile page and check status = 200 */
+        $crawler = $this->client->request('GET', '/user/show');
+
+        /* Check that response status is HTTP::OK */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("user/show")')->count()
+        );
+    }
+
+    /**
+     * Test to user/show action
+     */
+    public function testEdit()
+    {
+        /* LogIn to application */
+        $this->logIn();
+
+        /* Go to profile page and check status = 200 */
+        $crawler = $this->client->request('GET', '/user/edit');
+
+        /* Check that response status is HTTP::OK */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("user/edit")')->count()
+        );
+    }
+
+    /**
+     * Test to user/show action
+     */
+    public function testDelete()
+    {
+        /* LogIn to application */
+        $this->logIn();
+
+        /* Go to profile page and check status = 200 */
+        $crawler = $this->client->request('GET', '/user/delete');
+
+        /* Check that response status is HTTP::OK */
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+
+        /* Check that page after redirect contains search phrase */
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("user/delete")')->count()
+        );
+    }
+
 
 }
