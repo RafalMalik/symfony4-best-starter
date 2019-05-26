bc66d1a6a73a6581a111c306c2707608c6d63140
RafalMalik <rafalmalik.info@gmail.com>
2019-05-26 18:01:16 +0200

    Create UserType form.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 
 /**
@@ -31,9 +35,38 @@ class UserController extends AbstractController
      *
      * @todo Make action when admin can generate user,
      */
-    public function new() {
+    public function new(Request $request) {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            /**
+             * @todo Extract this code to Service
+             */
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $this->container->get('security.token_storage')->setToken($token);
+            $this->container->get('session')->set('_security_main', serialize($token));
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_index');
+        }
+
         return $this->render('user/new.html.twig', [
-            'controller_name' => 'UserController',
+            'form' => $form
         ]);
     }
 
