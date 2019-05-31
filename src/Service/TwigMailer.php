53a7b893876dcab1844093e339c0d579625d6919
RafalMalik <rafalmalik.info@gmail.com>
2019-05-31 22:01:36 +0200

    Works with TwigMailer.

---
+++ src/Service/TwigMailer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class TwigMailer
+{
+
+    /** @var Swift_Mailer $mailer */
+    private $mailer;
+
+    public function __construct(Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+
+    /**
+     * @param $subject
+     * @param $to
+     * @param $template
+     * @param array $parameters
+     * @return mixed
+     */
+    public function send($subject, $to, $template, array $parameters)
+    {
+        $message = (new Swift_Message($subject))
+            ->setFrom('send@example.com')
+            ->setTo($to)
+            ->setBody(
+                $this->renderView(
+                    $template,
+                    $parameters
+                ),
+                'text/html'
+            );
+
+        $this->mailer->send($message);
+    }
+
+}
