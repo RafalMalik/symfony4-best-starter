8a41b3291a572773d05a8d43cf5c1653b69ece84
RafalMalik <rafalmalik.info@gmail.com>
2019-06-19 20:39:37 +0200

    Works with mock.

--- tests/unit/UserManagerTest.php
+++ tests/unit/UserManagerTest.php
@@ -9,55 +9,80 @@ use App\Service\TwigMailer;
 use App\Service\UserManager;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserManagerTest extends TestCase
 {
 
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $em;
 
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $encoder;
 
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $tokenStorage;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $session;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mailer;
+
+    /**
+     * @var UserManager
+     */
     private $userManager;
 
-    private $em;
 
     protected function setUp()
     {
         $this->em = $this->createMock(EntityManagerInterface::class);
-        $encoder = $this->createMock(UserPasswordEncoderInterface::class);
+        $this->encoder = $this->createMock(UserPasswordEncoderInterface::class);
         $tokenStorage = $this->createMock(TokenStorageInterface::class);
         $session = $this->createMock(SessionInterface::class);
         $mailer = $this->createMock(TwigMailer::class);
 
-
-
-        $this->userManager = new UserManager($this->em, $encoder, $tokenStorage, $session, $mailer);
+        $this->userManager = new UserManager(
+            $this->em,
+            $this->encoder,
+            $tokenStorage,
+            $session,
+            $mailer
+        );
     }
 
 
     public function testCreate()
     {
-
-        //$this->userManager = $this->createMock(UserManager::class);
-
         $user = new User();
-        $user->setPlainPassword('12345')
+        $user->setPlainPassword('abcdefgh')
             ->setEmail('create@o2.pl');
 
-        $this->userManager->create($user);
+        $this->encoder->expects($this->once())
+            ->method('encodePassword')
+            ->with($user, 'abcdefgh')
+            ->will($this->returnValue('array'));
 
         $this->em->expects($this->once())
-            ->method('create')
-            ->willReturn( array());
-//
-        $user = new User();
-        $user->setPlainPassword('12345')
-            ->setEmail('create@o2.pl');
-//
-//        $this->userManager->create($user);
+            ->method('persist')
+            ->with(User::class);
 
-
+        $this->userManager->create($user);
 
     }
 
