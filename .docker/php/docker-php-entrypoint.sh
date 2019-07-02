02529213862e6a7db9fa3d539f30a9f250249149
RafalMalik <rafalmalik.info@gmail.com>
2019-07-02 22:29:16 +0200

    Add Code Sniffer to docker image.

--- .docker/php/docker-php-entrypoint.sh
+++ .docker/php/docker-php-entrypoint.sh
@@ -6,4 +6,10 @@ php -d memory_limit=-1 /usr/local/bin/composer install --no-interaction
 # other stuff - deployer - tests - etc
 #./../deploy.sh
 
+### Symfony Coding Standard ###
+
+vendor/bin/phpcs --config-set installed_paths vendor/escapestudios/symfony2-coding-standard
+vendor/bin/phpcs -i
+#vendor/bin/phpcs .
+
 exec "$@";
