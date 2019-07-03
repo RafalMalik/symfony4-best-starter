cf7fcf436f3c3a7ff1547c8cc95f85f2cc826357
Rafal Malik <rafalmalik.info@gmail.com>
2019-07-03 18:44:54 +0200

    Add ApiPlatform.

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,6 @@ return [
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Ajgon\LintPackBundle\LintPackBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
